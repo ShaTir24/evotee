12b87cb40478cde2004f2969447f49db98185152
Tirth Shah <tirthshah100@gmail.com>
2023-07-31 23:38:37 +0530

    Add pages

---
+++ routes/dashboard.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard | eVotee</title>
+</head>
+<body>
+    <h1>eVotee</h1>
+    <button>Back</button>
+    <button>Logout<button>
+    <h2>Online Voting System</h2>
+    <hr>
+    <h3>Dashboard</h3>
+    <div id = "profile"></div>
+    <div id = "group"></div>
+</body>
+</html>
